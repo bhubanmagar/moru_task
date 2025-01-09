0b6a243b3ea7c14434c3d69ce3ead49ed0dadf91
smash3r <bhubanmagar79@gmail.com>
2025-01-09 18:07:20 +0545

    final product-1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -79,11 +79,11 @@ const HomePage = () => {
       </table>
 
       {/* Pagination */}
-      <div className="flex justify-center items-center mt-6">
+      <div className="flex flex-wrap justify-center items-center mt-6 gap-2">
         <button
           onClick={() => paginate(currentPage - 1)}
           disabled={currentPage === 1}
-          className="px-5 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 disabled:bg-gray-300 disabled:cursor-not-allowed transition duration-200 ease-in-out"
+          className="px-5 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-400 disabled:bg-gray-300 disabled:cursor-not-allowed transition duration-200 ease-in-out"
         >
           Prev
         </button>
@@ -93,7 +93,7 @@ const HomePage = () => {
           <button
             key={pageNumber}
             onClick={() => paginate(pageNumber)}
-            className={`px-3 py-2 mx-2 rounded-lg text-white ${
+            className={`px-3 py-1 mx-2 rounded-lg text-white ${
               currentPage === pageNumber
                 ? "bg-teal-500 hover:bg-teal-400"
                 : "bg-gray-300 hover:bg-gray-200"
@@ -106,7 +106,7 @@ const HomePage = () => {
         <button
           onClick={() => paginate(currentPage + 1)}
           disabled={currentPage === totalPages}
-          className="px-5 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-400 disabled:bg-gray-300 disabled:cursor-not-allowed transition duration-200 ease-in-out"
+          className="px-5 py-1 bg-blue-500 text-white rounded-lg hover:bg-blue-400 disabled:bg-gray-300 disabled:cursor-not-allowed transition duration-200 ease-in-out"
         >
           Next
         </button>
